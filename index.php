41e15b0df4353a3eec044da7542cd237ec520e80
Abhinav Khanna <akhanna@princeton.edu>
2015-11-16 13:01:19 -0500

    updated

--- index.php
+++ index.php
@@ -117,7 +117,7 @@
         <button type="submit" class="btn btn-default">Submit</button>
       </p>
     </form> -->
-    <iframe src="https://docs.google.com/forms/d/11RPHUOtZcl8bLSALVBR1zq3REY8usQG1-ezdPIns1bg/viewform?embedded=true" width="600" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+    <iframe src="https://docs.google.com/forms/d/11RPHUOtZcl8bLSALVBR1zq3REY8usQG1-ezdPIns1bg/viewform?embedded=true" width="100%" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
   </div>
 
 <?php
