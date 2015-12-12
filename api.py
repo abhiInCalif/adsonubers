12952ef38b77a2b3fef09d24f5bc7836fc452e58
Abhinav Khanna <akhanna@princeton.edu>
2015-12-12 16:03:10 -0500

    updated

--- api.py
+++ api.py
@@ -1,17 +1,48 @@
 __author__ = 'abkhanna'
 
 import web
+from pymongo import MongoClient
+
 
 urls = (
     '/', 'Index',
+    '/drivers', 'Drivers',
 )
 
 render = web.template.render('templates')
+
+def database():
+    client = MongoClient("mongodb://admin:admin@ds027155.mongolab.com:27155/heroku_sqqktg3l")
+    db = client.heroku_sqqktg3l
+    return db
 
 class Index:
     def GET(self):
         # showcase the index page.
         return render.home()
+
+class Drivers:
+    def GET(self):
+        return render.drivers()
+
+    def POST(self):
+        import pdb; pdb.set_trace()
+        request_data = web.input(first='', last='', email='', phone='', city='', miles='')
+        first = request_data.first
+        last = request_data.last
+        email = request_data.email
+        phone = request_data.phone
+        city = request_data.city
+        miles = request_data.miles
+        database().DriverRecord.insert_one({
+            "first_name": first,
+            "last_name": last,
+            "email": email,
+            "phone": phone,
+            "city": city,
+            "miles": miles
+        })
+        return render.drivers()
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
